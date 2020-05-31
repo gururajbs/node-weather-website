87a88f56b6046c01bcec43521d8e393ba27b65c1
Gururaj Bharadwaj <gbharadwaj@synamedia.com>
2020-05-31 11:44:16 +0530

    Adding about text and Changed wether display data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=a41777955c32dd06270ef3ec7629e252&query=' + latitude +',' + longitude +'&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=a41777955c32dd06270ef3ec7629e252&query=' + latitude +',' + longitude;
 
     request({url, json: true}, function (error, response, body) { 
         if(error) {
@@ -10,9 +10,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Location data incorrect!', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
-            ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+            ' °C. It feels like ' + body.current.feelslike + ' °C. ' + 'Humidity is ' + body.current.humidity + '%.');
         }
-        
     })
 };
 
